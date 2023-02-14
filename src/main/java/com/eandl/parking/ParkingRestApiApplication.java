a5a68a713f879df9774d5f78a463278b703640a6
Edwin J. Páez <edwinjpr11@gmail.com>
2023-02-14 18:39:51 -0500

    feat(project): create initial project with initializr
    n

---
+++ src/main/java/com/eandl/parking/ParkingRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.eandl.parking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkingRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eandl/parking/ParkingRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eandl.parking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
