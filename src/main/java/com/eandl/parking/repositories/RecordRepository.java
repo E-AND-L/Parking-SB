f64200f1a295ced220144b0ac3883aa9e8a3b868
Edwin J. Páez <edwinjpr11@gmail.com>
2023-02-14 21:32:19 -0500

    feat(repositories): create repositories with jpa

---
+++ src/main/java/com/eandl/parking/repositories/RecordRepository.java
@@ -0,0 +1,9 @@
+package com.eandl.parking.repositories;
+
+import com.eandl.parking.models.Record;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RecordRepository extends JpaRepository<Record, Long> {
+}
---
+++ src/main/java/com/eandl/parking/repositories/VehicleRepository.java
@@ -0,0 +1,9 @@
+package com.eandl.parking.repositories;
+
+import com.eandl.parking.models.Vehicle;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface VehicleRepository extends JpaRepository<Vehicle, Long> {
+}
